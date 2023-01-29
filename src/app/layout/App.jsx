7905bcad26609d57220937b0a35b79d6db0fac7d
Jon Doe <jondoe@doe.com>
2023-01-29 17:47:21 +0800

    basic setup

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+
+export default function App() {
+  return (
+    <div className="App">    
+        <h1>Re-vents</h1>
+        <EventDashboard />
+    </div>
+  );
+}
+
+
