228a4cf819ae24de9fd22b8c145753b727a5f224
Jon Doe <jondoe@doe.com>
2023-01-30 10:37:26 +0800

    step5- passing the params to child component(event sample data)

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,12 +1,13 @@
 import { Grid } from "semantic-ui-react"
 import EventForm from "../eventForm/EventForm"
 import EventList from "./EventList"
+import { sampleData } from '../../../app/api/sampleData'
 
 export default function EventDashboard() {
     return(
         <Grid>
             <Grid.Column width="10">
-                <EventList />
+                <EventList events={sampleData} />
             </Grid.Column>
             <Grid.Column width="6">
                 <EventForm />
